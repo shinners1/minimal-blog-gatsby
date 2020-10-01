ba7f09c0ebef557b94874cc27dacbf521acf3883
shinners1 <49718885+shinners1@users.noreply.github.com>
2020-10-01 13:20:53 +0900

    test4

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,10 @@ module.exports = {
             title: `About`,
             slug: `/about`,
           },
+          {
+            title: `Study`,
+            slug: `/study`,
+          },
         ],
         externalLinks: [
           {
@@ -47,7 +51,7 @@ module.exports = {
       options: {
         name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
         short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        description: `code highlighting.`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
